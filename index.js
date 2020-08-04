292b835ed08607efaa4b94668fd930f15e20579c
roukell <62136980+roukell@users.noreply.github.com>
2020-08-04 13:54:05 +1000

    created mysql connection

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const inquirer = require("inquirer");
+const prompt = inquirer.createPromptModule();
 const fs = require("fs");
 const mysql = require("mysql");
-const prompt = inquirer.createPromptModule();
+const cTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "employee_db"
+  });
+  
+  connection.connect(err => {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+  });

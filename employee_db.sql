648fe5bd213199005c8f92994833265437aa4947
roukell <62136980+roukell@users.noreply.github.com>
2020-08-04 12:36:25 +1000

    added employee_db

---
+++ employee_db.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+  id INT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(6,2) NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);

f33c35a93613aee3fd47657cb319e1b302f999be
roukell <62136980+roukell@users.noreply.github.com>
2020-08-09 00:13:14 +1000

    update csv data due to removal of code column

--- employee_db.sql
+++ employee_db.sql
@@ -20,7 +20,6 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
   id INT NOT NULL,
-  code VARCHAR(30) NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
